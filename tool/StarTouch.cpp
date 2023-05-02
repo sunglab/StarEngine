d1e2648fc44add64419d08057d85b80b03ecc531
Sung <sunglab@gmail.com>
2023-05-02 17:17:02 +0900

    chore :  removed TUIO code
    TUIO is no longer used here so removed.

--- tool/StarTouch.cpp
+++ tool/StarTouch.cpp
@@ -9,150 +9,18 @@
 
 #include "StarTouch.h"
 
+StarTouch::StarTouch(StarTouchDelegate* _delegate, Vec2 rect, bool TUIO) {
 
-//StarTouch::StarTouch(StarTouchDelegate* _delegate,bool TUIO)
-StarTouch::StarTouch(StarTouchDelegate* _delegate,Vec2 rect, bool TUIO)
-{
-    
-    delegate = _delegate; init(TUIO);
+  delegate = _delegate;
+  init(TUIO);
 }
 
-void StarTouch::init(bool TUIO)
-{
-#if (MAC && !StarQt)
-    if(TUIO)
-        StarTUIO* app = new StarTUIO(3333,this);
-    //    app->run();
-    //    delete app;
-#endif
-    
-    for(int i = 0;i<MAX_FINGERS;i++)
-    {
-        nowPos[i].zero();
-        prePos[i].zero();
-        end = true;
-    }
-    fingers = 0;
-}
+void StarTouch::init(bool TUIO) {
 
-#if (MAC && !StarQt)
-void StarTUIO::addTuioObject(TuioObject *tobj) {
-    if (verbose)
-        std::cout << "add obj " << tobj->getSymbolID() << " (" << tobj->getSessionID() << "/"<<  tobj->getTuioSourceID() << ") "<< tobj->getX() << " " << tobj->getY() << " " << tobj->getAngle() << std::endl;
+  for (int i = 0; i < MAX_FINGERS; i++) {
+    nowPos[i].zero();
+    prePos[i].zero();
+    end = true;
+  }
+  fingers = 0;
 }
-
-void StarTUIO::updateTuioObject(TuioObject *tobj) {
-    if (verbose)
-        std::cout << "set obj " << tobj->getSymbolID() << " (" << tobj->getSessionID() << "/"<<  tobj->getTuioSourceID() << ") "<< tobj->getX() << " " << tobj->getY() << " " << tobj->getAngle()
-        << " " << tobj->getMotionSpeed() << " " << tobj->getRotationSpeed() << " " << tobj->getMotionAccel() << " " << tobj->getRotationAccel() << std::endl;
-}
-
-void StarTUIO::removeTuioObject(TuioObject *tobj) {
-    
-    if (verbose)
-        std::cout << "del obj " << tobj->getSymbolID() << " (" << tobj->getSessionID() << "/"<<  tobj->getTuioSourceID() << ")" << std::endl;
-}
-
-void StarTUIO::addTuioCursor(TuioCursor *tcur) {
-    
-    if (verbose)
-        std::cout << "add cur " << tcur->getCursorID() << " (" <<  tcur->getSessionID() << "/"<<  tcur->getTuioSourceID() << ") " << tcur->getX() << " " << tcur->getY() << std::endl;
-    
-    if(tcur->getCursorID()<10)
-    {
-        float xpos  = tcur->getPosition().getX();
-        float ypos  = tcur->getPosition().getY();
-        float starWidth = startouch->starRect.x;
-        float starHeight = startouch->starRect.y;
-        startouch->prePos[tcur->getCursorID()] = startouch->nowPos[tcur->getCursorID()];
-        startouch->nowPos[tcur->getCursorID()] = Vec2(starWidth*xpos,starHeight-starHeight*ypos);
-        
-    }
-    
-    startouch->callbackBegin();
-}
-
-void StarTUIO::updateTuioCursor(TuioCursor *tcur) {
-    
-    if (verbose)
-        std::cout << "set cur " << tcur->getCursorID() << " (" <<  tcur->getSessionID() << "/"<<  tcur->getTuioSourceID() << ") " << tcur->getX() << " " << tcur->getY()
-        << " " << tcur->getMotionSpeed() << " " << tcur->getMotionAccel() << " " << std::endl;
-    std::list<TuioCursor*> cursorList = tuioClient->getTuioCursors();
-    tuioClient->lockCursorList();
-    int i = 0;
-    for (std::list<TuioCursor*>::iterator iter = cursorList.begin(); iter!=cursorList.end(); iter++,i++) {
-        TuioCursor *tuioCursor = (*iter);
-        float xpos  = tuioCursor->getPosition().getX();
-        float ypos  = tuioCursor->getPosition().getY();
-        float starWidth = startouch->starRect.x;
-        float starHeight = startouch->starRect.y;
-        int index = tuioCursor->getCursorID();
-//        printf("xpos:%f ypos:%f index:%d\n", xpos, ypos,index);
-        startouch->prePos[index] = startouch->nowPos[index];
-        startouch->nowPos[index] = Vec2(starWidth*xpos,starHeight-starHeight*ypos);
-    }
-    
-    //    printf("i = %d", i);
-    startouch->fingers = i;
-    startouch->end = false;
-    tuioClient->unlockCursorList();
-    startouch->callbackMove();
-}
-
-void StarTUIO::removeTuioCursor(TuioCursor *tcur) {
-    
-     startouch->callbackEnd();// to notice how many fingers
-    
-    if (verbose)
-        std::cout << "del cur " << tcur->getCursorID() << " (" <<  tcur->getSessionID() << "/"<<  tcur->getTuioSourceID() << ")" << std::endl;
-    if(tcur->getCursorID()==0)
-    {   //printf("why false?\n");
-         startouch->end = true;
-        startouch->fingers = 0;
-    }
-    
-}
-
-void StarTUIO::addTuioBlob(TuioBlob *tblb) {
-    
-    if (verbose)
-        std::cout << "add blb " << tblb->getBlobID() << " (" << tblb->getSessionID()  << "/"<<  tblb->getTuioSourceID()<< ") "<< tblb->getX() << " " << tblb->getY() << " " << tblb->getAngle() << " " << tblb->getWidth() << " " << tblb->getHeight() << " " << tblb->getArea() << std::endl;
-}
-
-void StarTUIO::updateTuioBlob(TuioBlob *tblb) {
-    
-    if (verbose)
-        std::cout << "set blb " << tblb->getBlobID() << " (" << tblb->getSessionID() << "/"<<  tblb->getTuioSourceID() << ") "<< tblb->getX() << " " << tblb->getY() << " " << tblb->getAngle() << " "<< tblb->getWidth() << " " << tblb->getHeight() << " " << tblb->getArea()
-        << " " << tblb->getMotionSpeed() << " " << tblb->getRotationSpeed() << " " << tblb->getMotionAccel() << " " << tblb->getRotationAccel() << std::endl;
-}
-
-void StarTUIO::removeTuioBlob(TuioBlob *tblb) {
-    
-    if (verbose)
-        std::cout << "del blb " << tblb->getBlobID() << " (" << tblb->getSessionID() << "/"<<  tblb->getTuioSourceID() << ")" << std::endl;
-}
-
-
-void StarTUIO::refresh(TuioTime frameTime) {
-}
-
-void StarTUIO::run()
-{
-        running=true;
-        while (running)
-        {
-            std::list<TuioCursor*> cursorList = tuioClient->getTuioCursors();
-            tuioClient->lockCursorList();
-            for (std::list<TuioCursor*>::iterator iter = cursorList.begin(); iter!=cursorList.end(); iter++) {
-                TuioCursor *tuioCursor = (*iter);
-                float xpos  = tuioCursor->getPosition().getX();
-                float ypos  = tuioCursor->getPosition().getY();
-                int index = tuioCursor->getCursorID();
-                printf("xpos:%f ypos:%f index:%d\n", xpos, ypos,index);
-                startouch->nowPos[index] = Vec2(xpos,ypos);
-                
-            }
-            tuioClient->unlockCursorList();
-        }
-}
-#endif
--- tool/StarTouch.h
+++ tool/StarTouch.h
@@ -10,103 +10,45 @@
 
 #include "../math/StarMath.h"
 
-#if (MAC && !StarQt)
-#include "TuioListener.h"
-#include "TuioClient.h"
-#include "UdpReceiver.h"
-#include "TcpReceiver.h"
-#include <list>
-#endif
-
 #define MAX_FINGERS 10
 
-class StarTouchDelegate
-{
-    public:
-    virtual void CallbackTouchEnd() = 0;
-    virtual void CallbackTouchBegin() = 0;
-    virtual void CallbackTouchMove() = 0;
-    virtual void CallbackTouchCancel() = 0;
+class StarTouchDelegate {
+ public:
+  virtual void CallbackTouchEnd() = 0;
+  virtual void CallbackTouchBegin() = 0;
+  virtual void CallbackTouchMove() = 0;
+  virtual void CallbackTouchCancel() = 0;
 };
 
 class StarTUIO;
-class StarTouch
-{
-    public:
-    
-    StarTouchDelegate* delegate;
-    
-    Vec2 starRect;
-	Vec2 nowPos[MAX_FINGERS];
-	Vec2 prePos[MAX_FINGERS];
-	unsigned int fingers;
-	bool end;
-    
-    StarTouch(StarTouchDelegate* _delegate,
-              Vec2 _starRect = 0,
-              bool TUIO = true);
-    
-    void init(bool TUIO = true);
+class StarTouch {
+ public:
+  StarTouchDelegate* delegate;
 
-	StarTouch* clear()
-	{
-		fingers = 0;
-		for (int i = 0; i < MAX_FINGERS; i++)
-		{
-			nowPos[i].zero();
-			prePos[i].zero();
-			end = true;
-		}
-		return this;
-	};
+  Vec2 starRect;
+  Vec2 nowPos[MAX_FINGERS];
+  Vec2 prePos[MAX_FINGERS];
+  unsigned int fingers;
+  bool end;
 
-    void callbackBegin(){delegate->CallbackTouchBegin();}
-    void callbackMove(){delegate->CallbackTouchMove();}
-    void callbackEnd(){delegate->CallbackTouchEnd();}
-    void callbackCancel(){delegate->CallbackTouchCancel();}
+  StarTouch(StarTouchDelegate* _delegate, Vec2 _starRect = 0, bool TUIO = true);
+
+  void init(bool TUIO = true);
+
+  StarTouch* clear() {
+    fingers = 0;
+    for (int i = 0; i < MAX_FINGERS; i++) {
+      nowPos[i].zero();
+      prePos[i].zero();
+      end = true;
+    }
+    return this;
+  };
+
+  void callbackBegin() { delegate->CallbackTouchBegin(); }
+  void callbackMove() { delegate->CallbackTouchMove(); }
+  void callbackEnd() { delegate->CallbackTouchEnd(); }
+  void callbackCancel() { delegate->CallbackTouchCancel(); }
 };
 
-#if (MAC && !StarQt)
-using namespace TUIO;
-class StarTUIO : public TuioListener {
-    
-public:
-    StarTUIO(int port,StarTouch* _startouch): verbose (false),running(false)
-    {
-        osc_receiver = new UdpReceiver(3333);
-        tuioClient = new TuioClient(osc_receiver);
-        tuioClient->addTuioListener(this);
-        tuioClient->connect();
-        this->startouch = _startouch;
-    }
-    ~StarTUIO() {
-        tuioClient->disconnect();
-        delete tuioClient;
-        delete osc_receiver;
-    }
-    
-    void addTuioObject(TuioObject *tobj);
-    void updateTuioObject(TuioObject *tobj);
-    void removeTuioObject(TuioObject *tobj);
-    
-    void addTuioCursor(TuioCursor *tcur);
-    void updateTuioCursor(TuioCursor *tcur);
-    void removeTuioCursor(TuioCursor *tcur);
-    
-    void addTuioBlob(TuioBlob *tblb);
-    void updateTuioBlob(TuioBlob *tblb);
-    void removeTuioBlob(TuioBlob *tblb);
-    
-    void refresh(TuioTime frameTime);
-    
-    void run();
-    
-private:
-    bool verbose;
-    bool running;
-    TuioClient *tuioClient;
-    OscReceiver *osc_receiver;
-    StarTouch *startouch;
-};
-#endif
 #endif
